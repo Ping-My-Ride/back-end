cb92183966d083f1fc71dfdb0dba4466a2b149d9
Mauricio Lopez-Soto <mlopezsoto@gmail.com>
2018-09-03 18:14:23 -0500

    CORS

---
+++ src/main/java/com/endava/pingmyride/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.endava.pingmyride;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.cors().disable();
+    }
+}
